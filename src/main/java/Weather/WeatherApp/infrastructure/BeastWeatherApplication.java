beca2346d9a6a127a00bc7e061d9ed036b913268
William Anputra <wil_i_am1993@hotmail.com>
2017-02-14 20:59:35 +0700

    Update BeastWeatherApplication.java

--- src/main/java/Weather/WeatherApp/infrastructure/BeastWeatherApplication.java
+++ src/main/java/Weather/WeatherApp/infrastructure/BeastWeatherApplication.java
@@ -7,9 +7,9 @@ import com.firebase.client.Firebase;
 public class BeastWeatherApplication extends Application {
 
    public static final String BASE_WEATHER_URL = "http://api.openweathermap.org";
-   public static final String BASE_WEAHTER_API = "4a54bc57964e08a9e570f80dfc1bf73b";
+   public static final String BASE_WEAHTER_API = "Your Weather API";
 
-   public static final String BASE_FIRE_BASE_URL = "https://beastweather.firebaseio.com/data/";
+   public static final String BASE_FIRE_BASE_URL = "BaseURL";
 
 
    public static final String LOCATION_PREFERENCE = "LOCATION_PREFERENCE";
@@ -18,7 +18,7 @@ public class BeastWeatherApplication extends Application {
    public static final String BASE_WEATHER_IMAGE = "http://openweathermap.org/img/w/";
 
    public static final String APP_ID_PREFERENCE = "APP_ID_PREFERENCE";
-   public static final String APP_ID = "AIzaSyCSTVzqtbLXTuFo4yL3j1NT0yrt4l8Qc9o";
+   public static final String APP_ID = "Your APP id";
 
 
    @Override
